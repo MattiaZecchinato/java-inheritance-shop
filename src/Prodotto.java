0f91c3a0b117d54ecd3b1ee21dd1c5005a7011f5
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-07-03 15:29:10 +0200

    feat: add attributes, methods and constructor for Prodotto class

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,3 +1,53 @@
-public class Prodotto {
+import java.math.BigDecimal;
 
+public class Prodotto {
+    protected String code;
+    protected String name;
+    protected String brand;
+    protected BigDecimal price;
+    protected BigDecimal iva;
+
+    public Prodotto(String code, String name, String brand, BigDecimal price, BigDecimal iva) {
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getIva() {
+        return this.iva;
+    }
+
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
+    }
 }
